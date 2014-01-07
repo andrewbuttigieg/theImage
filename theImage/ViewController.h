1b143fa85ecce5cb64951d997d84e73620fdb326
Andrew Buttigieg <andrewbuttigieg@Andrews-Mac.local>
2014-01-07 21:27:26 +0100

    lot of new feature

--- theImage/ViewController.h
+++ theImage/ViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "ViewControllerDetail.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<CLLocationManagerDelegate, ViewControllerDetailDelegate> {
+    IBOutlet UIImageView *playerImage;
+    IBOutlet UILabel *xLok;
+    IBOutlet UILabel *yLok;
+    IBOutlet UILabel *playerOnline;
+    IBOutlet UILabel *playerName;
+    /*IBOutlet UIWebView *videoView;
+    NSString *videoURL;
+    NSString *videoHTML;*/
+}
+
+@property (nonatomic) NSString *returnedItem;
+
+/*@property(nonatomic, retain) IBOutlet UIWebView *videoView;
+@property(nonatomic, retain) NSString *videoURL;
+@property(nonatomic, retain) NSString *videoHTML;*/
+
+- (IBAction)logoPressed:(id)sender;
+- (IBAction)logoPressedTwo:(id)sender;
+- (IBAction)getHTTP:(id)sender;
 
 @end
--- theImage/ViewController.m
+++ theImage/ViewController.m
@@ -7,16 +7,30 @@
 //
 
 #import "ViewController.h"
+#import "ViewControllerDetail.h"
 
-@interface ViewController ()
+@interface ViewController() // <ViewControllerDetailDelegate>
 
 @end
 
 @implementation ViewController
 
+    CLLocationManager *locationManager;
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    locationManager = [[CLLocationManager alloc] init];
+    
+    playerOnline.text = @"Player Online";
+    playerName.text = @"ANDREW BUTTIGIEG";
+  /*
+    NSURL *url = [NSURL URLWithString:@"http://www.youtube.com/watch?v=fDXWW5vX-64"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+*/
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +38,117 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)logoPressedTwo:(id)sender {
+    NSLog(@"Pressed!");
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [locationManager startUpdatingLocation];
+}
+
+- (IBAction)getHTTP:(id)sender {
+    
+  //  NSLog(@"Pressed!");
+    NSURL *url = [NSURL URLWithString:@"http://techreport.com/news/25864/amd-sheds-more-light-on-kaveri-announces-new-mobile-radeons"];
+    
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url
+                                
+                                                cachePolicy:NSURLRequestReturnCacheDataElseLoad
+                                            timeoutInterval:30];
+    // Fetch the JSON response
+    NSData *urlData;
+    NSURLResponse *response;
+    NSError *error;
+    
+    // Make synchronous request
+    urlData = [NSURLConnection sendSynchronousRequest:urlRequest
+                                    returningResponse:&response
+                                                error:&error];
+    
+    // Construct a String around the Data from the response
+    NSString *http = [[NSString alloc] initWithData:urlData encoding:NSUTF8StringEncoding];
+    
+    /*
+     [NSURLConnection sendAsynchronousRequest:[[NSURLRequest alloc] initWithURL:url] queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+     
+     if (error) {
+     [self.delegate fetchingGroupsFailedWithError:error];
+     } else {
+     [self.delegate receivedGroupsJSON:data];
+     }
+     }];
+     
+     */
+
+}
+
+- (IBAction)logoPressed:(id)sender {
+    /*
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [locationManager startUpdatingLocation];
+     */
+   NSLog(@"Pressed!");
+   /* UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Good one!"
+message:@"You hit the logo"
+delegate:self
+cancelButtonTitle:@"Go Away"
+otherButtonTitles:nil];
+    [alert show];*/
+    
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+	if ([segue.identifier isEqualToString:@"AddPlayer"])
+	{
+		UINavigationController *navigationController =
+        segue.destinationViewController;
+		ViewControllerDetail
+        *ViewControllerDetail = [[navigationController viewControllers]
+objectAtIndex:0];
+		ViewControllerDetail.delegate = self;
+	}
+}
+
+#pragma mark - ViewControllerDetailDelegate
+
+- (void)playerDetailsViewControllerDidSave:
+(ViewControllerDetail *)controller
+{
+    NSLog(@"End me");
+	[self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)addItemViewController:
+(ViewControllerDetail *)controller didFinishEnteringItem:    (NSString *)item
+{
+    //using delegate method, get data back from second page view controller and set it to property declared in here
+    NSLog(@"This was returned from secondPageViewController: %@",item);
+    self.returnedItem=item;
+    
+    //add item to array here and call reload
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"didUpdateToLocation: %@", newLocation);
+    CLLocation *currentLocation = newLocation;
+    
+    if (currentLocation != nil) {
+        xLok.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+        yLok.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
+    }
 }
 
 @end
