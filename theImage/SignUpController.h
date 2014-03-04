b95a5b119a2c2e8547a62f58371c36c6dd5b2b20
andrewbuttigieg <andrewbuttigieg@gmail.com>
2014-03-04 22:59:13 +0100

    begun work on Sign Up page

---
+++ theImage/SignUpController.h
@@ -0,0 +1,23 @@
+//
+//  SignUpController.h
+//  theImage
+//
+//  Created by Andrew Buttigieg on 3/4/14.
+//  Copyright (c) 2014 PlayerCV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *accountType;
+@property (strong, nonatomic) IBOutlet UITextField *name;
+
+@property (strong, nonatomic) IBOutlet UITextField *lastName;
+@property (strong, nonatomic) IBOutlet UITextField *weight;
+@property (strong, nonatomic) IBOutlet UITextField *email;
+@property (strong, nonatomic) IBOutlet UITextField *password;
+
+- (IBAction)Done:(id)sender;
+
+@end
---
+++ theImage/SignUpController.m
@@ -0,0 +1,40 @@
+//
+//  SignUpController.m
+//  theImage
+//
+//  Created by Andrew Buttigieg on 3/4/14.
+//  Copyright (c) 2014 PlayerCV. All rights reserved.
+//
+
+#import "SignUpController.h"
+
+@interface SignUpController ()
+
+@end
+
+@implementation SignUpController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)Done:(id)sender {
+}
+@end
