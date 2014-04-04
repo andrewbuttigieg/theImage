723f35024a86264375e6e5366301973dd81e70f9
andrewbuttigieg <andrewbuttigieg@gmail.com>
2014-04-04 06:22:17 +0200

    facebook

--- theImage/SignUpController.m
+++ theImage/SignUpController.m
@@ -34,9 +34,9 @@ bool moved = false;
     
     FBLoginView *loginView = [[FBLoginView alloc] init];
     // Align the button in the center horizontally
-    loginView.readPermissions = @[@"basic_info", @"email", @"user_likes"];
+    loginView.readPermissions = @[@"basic_info", @"email"];
     loginView.delegate = self;
-    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 80);
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 20);
     [self.scrollView addSubview:loginView];
     
 //    self.activeTextField = self.password;
@@ -67,8 +67,46 @@ bool moved = false;
 - (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
                             user:(id<FBGraphUser>)user {
     
-    NSString *facebookPlayerID = user.id;
     NSLog(@"%@", user);
+   
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://newfootballers.com/reg_player.php/"]];
+    [request setValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
+    [request setHTTPBody:
+     [[NSString stringWithFormat:@"password=%@&email=%@&name=%@&lname=%@&weight=%dusertype=%d&facebookID=%@&gender=%@",
+                           @"", user[@"email"], user.first_name, user.last_name, 0, 1, user.id, user[@"gender"]]dataUsingEncoding:NSUTF8StringEncoding]];
+    [request setHTTPMethod:@"POST"];
+    NSError *error = nil; NSURLResponse *response = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if (error) {
+        NSLog(@"Error:%@", error.localizedDescription);
+    }
+    else {
+        //success
+        NSLog(@"%@", data);
+        
+        NSMutableArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data
+                                                                    options:0
+                                                                      error:&error];
+        NSLog(@"%@", jsonArray);
+        for(NSDictionary *dictionary in jsonArray)
+        {
+            //NSLog(@"Data Dictionary is : %@",jsonArray);
+            NSString *returned = [jsonArray[0] objectForKey:@"value"];
+            int accepted = [[jsonArray[0] objectForKey:@"accepted"] intValue];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (accepted == 0){
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Problem"
+                                                                    message:[NSString stringWithFormat:@"%@",returned]
+                                                                   delegate:self
+                                                          cancelButtonTitle:@"Ok"
+                                                          otherButtonTitles:nil];
+                    [alert show];
+                }
+            });
+        }
+    }
 }
 
 - (void)viewDidUnload{
@@ -85,24 +123,11 @@ bool moved = false;
     self.activeTextField = textField;
     
     if (moved ){
-    /*// Step 2: Adjust the bottom content inset of your scroll view by the keyboard height.
-        UIEdgeInsets contentInsets = UIEdgeInsetsMake(
-           self.scrollView.contentInset.top,
-           self.scrollView.contentInset.left, keyboardSize.height, 0.0);
-    
-        self.scrollView.contentInset = contentInsets;
-        self.scrollView.scrollIndicatorInsets = contentInsets;
-    */
-    // Step 3: Scroll the target text field into view.
         CGRect aRect = self.view.frame;
         aRect.size.height -= keyboardSize.height;
-    //if (!CGRectContainsPoint(aRect, self.activeTextField.frame.origin) ) {
-            CGPoint scrollPoint = CGPointMake(0.0, self.activeTextField.frame.origin.y - (keyboardSize.height-35));
-            [self.scrollView setContentOffset:scrollPoint animated:YES];
-            //        [self.scrollView scrollRectToVisible:scrollPoint animated:YES];
-   // }
+        CGPoint scrollPoint = CGPointMake(0.0, self.activeTextField.frame.origin.y - (keyboardSize.height-35));
+        [self.scrollView setContentOffset:scrollPoint animated:YES];
     }
-    
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
@@ -111,35 +136,13 @@ bool moved = false;
     
 }
 
-
 - (void) keyboardWillHide:(NSNotification *)notification {
-    
-    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
-    /*self.scrollView.contentInset = contentInsets;
-    self.scrollView.scrollIndicatorInsets = contentInsets;*/
-    
     moved = false;
 }
 
 - (void)keyboardWasShown:(NSNotification *)notification
 {
-    
-    // Step 1: Get the size of the keyboard.
     keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
-    
-   /* int top = self.scrollView.contentInset.top;
-    UIEdgeInsets contentInsets = UIEdgeInsetsMake(
-                                                  self.scrollView.contentInset.top,
-                                                  self.scrollView.contentInset.left, keyboardSize.height, 0.0);
-    // Step 2: Adjust the bottom content inset of your scroll view by the keyboard height.
-     contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0);
-    
-    contentInsets.top = top;
-    
-    self.scrollView.contentInset = contentInsets;
-    self.scrollView.scrollIndicatorInsets = contentInsets;*/
-    
-    // Step 3: Scroll the target text field into view.
     CGRect aRect = self.view.frame;
     aRect.size.height -= keyboardSize.height;
     CGRect rect = [[UIApplication sharedApplication] statusBarFrame];
